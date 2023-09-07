ce1a4796a3340636bab50a7be426087fc24c2162
sevbesau <seppe@sevbesau.xyz>
2023-09-07 12:32:05 +0200

    clean up plymouth

--- hosts/common/optional/quietboot.nix
+++ hosts/common/optional/quietboot.nix
@@ -6,15 +6,6 @@
   };
 
   boot = {
-    plymouth = {
-      enable = true;
-      theme = "spinner-monochrome";
-      themePackages = [
-        (pkgs.plymouth-spinner-monochrome.override {
-          inherit (config.boot.plymouth) logo;
-        })
-      ];
-    };
     loader.timeout = 0;
     kernelParams = [
       "quiet"
