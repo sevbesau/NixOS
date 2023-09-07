ae6beb5790e5f4300c3c0bbc7a98dc1d3b09009c
sevbesau <seppe@sevbesau.xyz>
2023-09-07 12:22:36 +0200

    install plymouth themes

--- hosts/switchblade/default.nix
+++ hosts/switchblade/default.nix
@@ -32,6 +32,16 @@
   # graphical boot
   boot.plymouth = {
     enable = true;
+    themePackages = with pkgs; {
+      adi1090x-plymouth-themes
+    };
+    # theme = "";
+    logo = {
+      pkgs.fetchurl {
+        url = "https://nixos.org/logo/nixos-hires.png";
+        sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
+      }
+    };
   };
 
   # This value determines the NixOS release from which the default
