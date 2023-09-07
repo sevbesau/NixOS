40a37678734997254db394d7c66bbcc2b8c627fb
sevbesau <seppe@sevbesau.xyz>
2023-09-07 12:35:09 +0200

    remove logo

--- hosts/common/optional/graphical-boot.nix
+++ hosts/common/optional/graphical-boot.nix
@@ -7,10 +7,5 @@
       adi1090x-plymouth-themes
     ];
     theme = "green_blocks";
-    logo = 
-      pkgs.fetchurl {
-        url = "https://nixos.org/logo/nixos-hires.png";
-        sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
-      };
   };
 }
