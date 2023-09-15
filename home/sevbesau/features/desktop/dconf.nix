fac4ff3f9a154a0a7c1b93a4cd6e085358d53607
sevbesau <seppe@sevbesau.xyz>
2023-09-15 10:55:23 +0200

    prefer dark mode

--- home/sevbesau/features/desktop/dconf.nix
+++ home/sevbesau/features/desktop/dconf.nix
@@ -54,7 +54,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/interface" = {
-      color-scheme = "default";
+      color-scheme = "prefer-dark";
       enable-hot-corners = false;
       show-battery-percentage = true;
     };
