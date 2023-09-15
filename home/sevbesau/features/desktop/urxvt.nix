a51a446e0c78f5df632f0b9dcd8938c147373338
sevbesau <seppe@sevbesau.xyz>
2023-09-15 10:46:47 +0200

    add emoji fonts

--- home/sevbesau/features/desktop/urxvt.nix
+++ home/sevbesau/features/desktop/urxvt.nix
@@ -1,7 +1,7 @@
 {
   programs.urxvt = {
     enable = true;
-    fonts = [ "xft:monospace:size=18" ];
+    fonts = [ "xft:monospace:size=18, xft:JoyPixels:size=18, Noto Color Emoji:size=18" ];
     scroll.bar.enable = false;
   };
 }
