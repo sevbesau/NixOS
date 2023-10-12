c04ac64ff1647924450a5c78cf6a2621ac43326a
sevbesau <seppe@sevbesau.xyz>
2023-10-12 16:59:49 +0200

    fix colorscheme

--- home/sevbesau/common/default.nix
+++ home/sevbesau/common/default.nix
@@ -21,7 +21,30 @@ in
     inputs.nix-colors.homeManagerModules.default
   ];
   
-  colorScheme = lib.mkDefault colorSchemes.monokai;
+  # colorScheme = lib.mkDefault colorSchemes.monokai;
+  colorScheme = {
+    slug = "monokai";
+    name = "Monokai";
+    author = "Wimer Hazenberg (http://www.monokai.nl)";
+    colors = {
+      base00 = "#272822";
+      base01 = "#f92672";
+      base02 = "#a6e22e";
+      base03 = "#f4bf75";
+      base04 = "#66d9ef";
+      base05 = "#ae81ff";
+      base06 = "#a1efe4";
+      base07 = "#f8f8f2";
+      base08 = "#75715e";
+      base09 = "#f92672";
+      base0A = "#a6e22e";
+      base0B = "#f4bf75";
+      base0C = "#66d9ef";
+      base0D = "#ae81ff";
+      base0E = "#a1efe4";
+      base0F = "#f9f8f5";
+    };
+  };
   
   nixpkgs.config = {
     allowUnfree = true;
